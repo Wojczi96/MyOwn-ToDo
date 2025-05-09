c28bcea52f62996581f87ba3fed37cd665c41549
Wojciech Wyskiel <wojtass96@MacBook-Air-Wojciech.local>
2025-05-09 07:34:15 +0200

    Some news

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,12 +1,29 @@
 import { Component, input } from '@angular/core';
+import { TaskComponent } from './task/task.component';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [],
+  imports: [TaskComponent, ReactiveFormsModule],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.scss'
 })
 export class TasksComponent {
   name = input<string>()
+  isOpen = false;
+  newTaskForm = new FormGroup({
+    title: new FormControl(''),
+    summary: new FormControl(''),
+    date: new FormControl('')
+  })
+
+
+  onOpenForm(){
+    this.isOpen = !this.isOpen;
+  }
+  onSumbit(){
+    console.log("Clicked")
+    console.log(this.newTaskForm.value)
+  }
 }
